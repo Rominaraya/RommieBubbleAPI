a7f4227405f4e6b6d30481ec2be5e8a366d83cc7
rominaraya <rarayaa@utem.cl>
2025-07-18 16:07:19 -0400

    inicio proyecto

---
+++ src/test/java/com/rommiebubble/RommieBubbleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rommiebubble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RommieBubbleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
